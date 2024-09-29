d5ab04acd8f8b4681c7da47323e4c177e1acf5a3
DavidGoodman2333 <davidgoodman2333@protonmail.com>
2024-09-29 14:32:13 +0000

    INIT

---
+++ infra/int/ap-southeast-1/aws_github_oidc.tf
@@ -0,0 +1,8 @@
+module "iam_github_oidc_provider" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+
+  tags = {
+    terraform-managed = true
+    environment       = "prod"
+  }
+}
