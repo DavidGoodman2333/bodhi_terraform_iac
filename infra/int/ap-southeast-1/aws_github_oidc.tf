31385638fbef92e3d842c0a4e9d7b33798303d27
Dawei Hao <haodawei@xiaiobing.ai>
2024-10-01 02:00:44 +0800

    MOD: to add a new bucket

--- infra/int/ap-southeast-1/aws_github_oidc.tf
+++
@@ -1,8 +0,0 @@
-module "iam_github_oidc_provider" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-
-  tags = {
-    terraform-managed = true
-    environment       = "int"
-  }
-}
