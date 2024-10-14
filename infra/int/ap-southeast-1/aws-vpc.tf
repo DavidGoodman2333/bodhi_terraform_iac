b8803ec49c7ca79a7cdc7f64837a6984f118ecbc
Dawei Hao <haodawei@xiaiobing.ai>
2024-10-14 17:21:09 +0800

    MOD: update available zone

--- infra/int/ap-southeast-1/aws-vpc.tf
+++ infra/int/ap-southeast-1/aws-vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   version                = "5.8.1"
   cidr                   = "10.0.0.0/16"
   secondary_cidr_blocks  = ["10.32.0.0/16"]
-  azs                    = ["ap-southeast-1"]
+  azs                    = ["ap-east-1a", "ap-east-1b", "ap-east-1c"]
   private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.32.0.0/18", "10.32.64.0/18", "10.32.128.0/18"]
   public_subnets         = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24", "10.32.192.0/24", "10.32.193.0/24", "10.32.194.0/24"]
   enable_nat_gateway     = true
