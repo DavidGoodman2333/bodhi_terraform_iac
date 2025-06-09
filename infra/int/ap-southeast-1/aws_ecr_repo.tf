f5713e69429dca0b6bccb0fbcd5b5f798f5d56fa
DavidGoodman2333 <davidgoodman2333@protonmail.com>
2025-06-09 18:23:00 +0000

    MOD: update

--- infra/int/ap-southeast-1/aws_ecr_repo.tf
+++ infra/int/ap-southeast-1/aws_ecr_repo.tf
@@ -1,7 +1,7 @@
-resource "aws_ecr_repository" "bodhi_frontend" {
-  name = "bodhi_frontend"
+resource "aws_ecr_repository" "msccoin_frontend" {
+  name = "msccoin_frontend"
 }
 
-resource "aws_ecr_repository" "bodhi_file_system" {
-  name = "bodhi_file_system"
+resource "aws_ecr_repository" "msccoin_file_system" {
+  name = "msccoin_file_system"
 }
--- infra/int/ap-southeast-1/aws_eks.tf
+++ infra/int/ap-southeast-1/aws_eks.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source                                   = "terraform-aws-modules/eks/aws"
   version                                  = "20.8.5"
-  cluster_name                             = "bodhi-int-eks"
+  cluster_name                             = "msccoin-int-eks"
   cluster_version                          = "1.29"
   cluster_endpoint_private_access          = false
   cluster_endpoint_public_access           = true
@@ -23,7 +23,7 @@ module "eks" {
       capacity_type  = "ON_DEMAND"
       subnet_ids     = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
       tags = {
-        "k8s.io/cluster-autoscaler/bodhi-int-eks" = "owned"
+        "k8s.io/cluster-autoscaler/msccoin-int-eks" = "owned"
         "k8s.io/cluster-autoscaler/enabled"           = "TRUE"
       }
       labels = {
@@ -40,7 +40,7 @@ module "eks" {
       capacity_type  = "ON_DEMAND"
       subnet_ids     = [module.vpc.private_subnets[3],module.vpc.private_subnets[4],module.vpc.private_subnets[5]]
       tags = {
-        "k8s.io/cluster-autoscaler/bodhi-int-eks" = "owned"
+        "k8s.io/cluster-autoscaler/msccoin-int-eks" = "owned"
         "k8s.io/cluster-autoscaler/enabled"           = "TRUE"
       }
       labels = {
@@ -55,42 +55,6 @@ module "eks" {
       protocol                      = "TCP"
       from_port                     = 15232
       to_port                       = 15232
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-
-    postgresql_service_ingress_rule_2 = {
-      description                   = "pg ingress 2"
-      protocol                      = "TCP"
-      from_port                     = 30970
-      to_port                       = 30970
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-
-    prometheus_service_ingress_rule = {
-      description                   = "prometheus ingress"
-      protocol                      = "TCP"
-      from_port                     = 9090
-      to_port                       = 9090
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-
-    node_exporter_ingress_rule = {
-      description                   = "node exporter ingress"
-      protocol                      = "TCP"
-      from_port                     = 9100
-      to_port                       = 9100
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-
-    olm_package_server_ingress_rule = {
-      description                   = "olm package server ingress"
-      protocol                      = "TCP"
-      from_port                     = 5443
-      to_port                       = 5443
       type                          = "ingress"
       source_cluster_security_group = true
     }
